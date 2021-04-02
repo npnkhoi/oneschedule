d55c4660f14e41aad728a8bef63fa1ebbd02e5fe
Khoi <khoinguyen.work@gmail.com>
2021-04-02 12:11:14 +0700

    change display of selected courses

--- src/components/SelectedCourses.js
+++ src/components/SelectedCourses.js
@@ -6,7 +6,10 @@ const SelectedCourses = ({courses, isSelected, isVisible, toggleVisibility, togg
       .filter((course) => isSelected(course.id))
       .map((course) => (
         <div className="selected-course d-flex flex-row justify-content-between align-items-center" key={course.id}> 
-          <div className="flex-grow-1 px-2">{course.course_name}</div>
+          <div className="selected-info flex-grow-1 pl-3 pr-2 d-flex flex-column">
+            <div>{course.course_name}</div>
+            <div class="selected-instructor mt-2">{course.instructor_name} ({course.id})</div>
+          </div>
 
           <div className="toggle-btns d-flex flew-row">
             <button 
@@ -14,9 +17,7 @@ const SelectedCourses = ({courses, isSelected, isVisible, toggleVisibility, togg
               onClick={() => toggleVisibility(course.id)}
             >
               {isVisible(course.id) ? <i className="fas fa-eye"></i> : <i className="fas fa-eye-slash"></i>}
-              
             </button>
-
             <button 
               className="btn"
               onClick={() => toggleSelection(course.id)}
