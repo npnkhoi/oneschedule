de2a22840d7a7477b2ddbbf5c1a5f560b8a619be
npnkhoi <npnkhoi@gmail.com>
2021-06-29 22:55:53 +0700

    update NavBar style

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,14 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import './NavBar.css'
 import logoSVG from '../assets/Logo.svg'
 
-const NavBtn = ({label, path, icon}) => (
-  <Link to={path} className='navbar-btn text-decoration-none align-self-stretch d-flex flex-column align-items-center p-2'>
-    <i className={`${icon} fa-2x mb-1`}/>
-    <p >{label}</p>
-  </Link>
-)
+const NavBtn = ({label, path, icon, currentView, setCurrentView}) => {
+  // const handleClick = () => {
+  //   setCurrentView(label)
+  //   console.log('setting currentView to', label);
+  // }
+  
+  return (
+    <Link to={path} 
+      className={`navbar-btn text-decoration-none align-self-stretch d-flex flex-column align-items-center p-2
+        ${(currentView === label ? 'current' : '')}
+      `}
+      onClick={() => {setCurrentView(label)}}
+    >
+      <i className={`${icon} fa-2x mb-1`}/>
+      <p >{label}</p>
+    </Link>
+  )
+}
 
 const Logo = () => (
   <Link to='/'>
@@ -17,14 +29,17 @@ const Logo = () => (
 )
 
 const NavBar = () => {
+  const [currentView, setCurrentView] = useState('Schedule')
+  // console.log('currentView:', currentView);
+  
   return (
     <div className='wrapper d-flex flex-column align-items-center text-center sticky-top'>
       <Logo />
-      <NavBtn label='Schedule' path='/' icon='far fa-calendar'/>
-      <NavBtn label='Courses' path='/courses' icon='far fa-list-alt'/>
-      <NavBtn label='Contribute' path='/contribute' icon='far fa-star'/>
-      <NavBtn label='Help' path='/help' icon='fas fa-question'/>
-      <NavBtn label='About' path='/about' icon='fas fa-info'/>
+      <NavBtn label='Schedule' path='/' icon='far fa-calendar' setCurrentView={setCurrentView} currentView={currentView}/>
+      <NavBtn label='Courses' path='/courses' icon='far fa-list-alt' setCurrentView={setCurrentView} currentView={currentView}/>
+      <NavBtn label='Contribute' path='/contribute' icon='far fa-star' setCurrentView={setCurrentView} currentView={currentView}/>
+      <NavBtn label='Help' path='/help' icon='fas fa-question' setCurrentView={setCurrentView} currentView={currentView}/>
+      <NavBtn label='About' path='/about' icon='fas fa-info' setCurrentView={setCurrentView} currentView={currentView}/>
     </div>
   )
 }
