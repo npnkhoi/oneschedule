8e8624392f54ecb76841fe9e4597803ae7a86fe3
npnkhoi <npnkhoi@gmail.com>
2021-07-01 16:19:19 +0700

    change to HashRouter

--- src/App.js
+++ src/App.js
@@ -4,13 +4,13 @@ import './components/Calendar.css';
 import React  from 'react'
 import { ToastContainer} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import {BrowserRouter as Router } from 'react-router-dom'
+import {HashRouter as Router } from 'react-router-dom'
 import NavBar from './components/NavBar';
 import Main from './components/Main';
 
 function App() {
   return (
-    <Router basename='/oneschedule'>
+    <Router>
       <div className="App d-flex flex-row">
         <NavBar />
         <Main/>
