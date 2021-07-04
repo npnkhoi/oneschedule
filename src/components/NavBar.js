3475d40d5a77277666469920f16d9938a9cbcea2
npnkhoi <npnkhoi@gmail.com>
2021-07-04 17:48:35 +0700

    make NavBar accurate in highlighting

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,15 +1,17 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react'
+import { Link, useLocation } from 'react-router-dom'
 import './NavBar.css'
 import logoSVG from '../assets/Logo.svg'
 
-const NavBtn = ({label, path, icon, currentView, setCurrentView}) => {
+const NavBtn = ({label, path, icon}) => {
+  const location = useLocation()
+  const currentView = location.pathname.split('/')[1]
+
   return (
     <Link to={path} 
       className={`navbar-btn text-decoration-none align-self-stretch d-flex flex-column align-items-center p-2
-        ${(currentView === label ? 'current' : '')}
+        ${('/' + currentView === path ? 'current' : '')}
       `}
-      onClick={() => {setCurrentView(label)}}
     >
       <i className={`${icon} fa-2x mb-1`}/>
       <p >{label}</p>
@@ -24,16 +26,14 @@ const Logo = () => (
 )
 
 const NavBar = () => {
-  const [currentView, setCurrentView] = useState('Schedule')
-  
   return (
     <div className='wrapper d-flex flex-column align-items-center text-center sticky-top'>
       <Logo />
-      <NavBtn label='Schedule' path='/' icon='far fa-calendar' setCurrentView={setCurrentView} currentView={currentView}/>
-      <NavBtn label='Offerings' path='/courses' icon='far fa-list-alt' setCurrentView={setCurrentView} currentView={currentView}/>
-      <NavBtn label='Contribute' path='/contribute' icon='far fa-star' setCurrentView={setCurrentView} currentView={currentView}/>
-      <NavBtn label='Help' path='/help' icon='fas fa-question' setCurrentView={setCurrentView} currentView={currentView}/>
-      <NavBtn label='About' path='/about' icon='fas fa-info' setCurrentView={setCurrentView} currentView={currentView}/>
+      <NavBtn label='Schedule' path='/' icon='far fa-calendar'/>
+      <NavBtn label='Offerings' path='/courses' icon='far fa-list-alt'/>
+      <NavBtn label='About' path='/about' icon='fas fa-info'/>
+      {/* <NavBtn label='Contribute' path='/contribute' icon='far fa-star'/>
+      <NavBtn label='Help' path='/help' icon='fas fa-question'/> */}
     </div>
   )
 }
