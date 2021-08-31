3c11188dbca18a9990882ebaa2a37b66866459cd
npnkhoi <npnkhoi@gmail.com>
2021-08-31 16:53:41 +0700

    shuffle colors, disable redux-reset behavior

--- src/store/index.js
+++ src/store/index.js
@@ -1,20 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit'
-import moment from 'moment'
-import config from '../utils/config'
 import { loadFromLocalStorage } from '../utils/localStorage'
 import selectedCoursesReducer from './selectedCoursesSlice'
 
-const currentVersion = localStorage.getItem('version')
-
-// FIXME
-if (currentVersion !== config.version) {
-  console.log('Deleting');
-  const oldData = localStorage.getItem('persistantState')
-  const timestamp = moment().format()
-  localStorage.setItem(timestamp, oldData)
-  localStorage.removeItem('persistantState')
-  localStorage.setItem('version', config.version)
-}
+// const currentVersion = localStorage.getItem('version')
+// if (currentVersion !== config.version) {
+//   console.log('Deleting');
+//   const oldData = localStorage.getItem('persistantState')
+//   const timestamp = moment().format()
+//   localStorage.setItem(timestamp, oldData)
+//   localStorage.removeItem('persistantState')
+//   localStorage.setItem('version', config.version)
+// }
 
 export default configureStore({
   reducer: {
