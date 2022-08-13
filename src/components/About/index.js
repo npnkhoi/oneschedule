9023662d8dd092563d888836f40ca57b6e85ba8b
npnkhoi <npnkhoi@gmail.com>
2022-08-13 10:49:07 +0700

    update info pages

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -12,7 +12,7 @@ const About = () => {
       <h2 className='my-4'>About this website</h2>
         <div className='d-flex flex-column flex-md-row align-items-center justify-content-between'>
           <div className=''>
-            <p>This website is created mainly by Khoi (me), a Fulbright CS student. I don&apos;t like copying class schedule to paper during course selection each semester. Therefore, I built OneSchedule to help <i>visualize schedules in one minute</i>.</p>
+            <p>This website is founded by Khoi, a Fulbright CS student. He doess&apos;t like copying class schedule to paper when selecting courses each semester. Therefore, he built <b>OneSchedule</b> to help visualize <b>schedules</b> in <b>one</b> minute.</p>
             <p>
               OneSchedule is not an official channel of Fulbright. It is free, <a href='https://github.com/npnkhoi/oneschedule'>open-source</a> and maintained by volunteers.
             </p>
@@ -24,14 +24,14 @@ const About = () => {
         <div>
         
         <h5>Our developers</h5>
-        <p>This app would not be possible without these dedicated programmers/friends of mine:</p>
+        <p>This app would not be possible without these dedicated programmers/friends of mine (Khoi):</p>
         <Contributors />
 
         <h5>Special thanks to</h5>
         
         <ul>
           <li>Le Thuc Minh Chau for being the first person to support this idea.</li>
-          <li> <a href='https://fullstackopen.com/en/'>Full Stack open course</a> from University of Helsinki for teaching everything about fullstack web development.</li>
+          <li> <a href='https://fullstackopen.com/en/'>Full Stack open course</a> from University of Helsinki for teaching Khoi everything about fullstack web development.</li>
           <li>The kind reviewers -- Huyen Nguyen, Tuong Minh, Binh Giang, chi Duyen (Ann), Dan Tam & Huy Vu, anh Vu Le, and more -- for giving their quality feedback.</li>
         </ul>
       </div>
--- src/components/Contribute/index.js
+++ src/components/Contribute/index.js
@@ -9,35 +9,43 @@ const Contribute = () => {
         <title>Contribute</title>
       </Helmet>
       <h2 className='mb-4'>Contribute to OneSchedule</h2>
-      <div className='text-start row g-3'>
+      <div className='text-start'>
       
-        <div className="rounded border p-3">
-          <h5>1. Donate</h5>
-          <p>
-            OneSchedule needs financial support to deliver more features: nicer domain, classroom map, credit planning, course review, and maybe an ecosystem of apps for Fulbright students.
-          </p>
-          <p>You can donate to OneSchedule via 2 following channels:</p>
-          <div>
-            <ol>
-              <li>ACB: 7132717 (NGUYEN PHUNG NHAT KHOI)
-              </li>
+        <h5>1. Donate</h5>
+        <p>
+          OneSchedule is free, and will continue to be so. However, it is built entirely by volunteers, who are also busy humans. 
+          Therefore, any donation -- big or small -- will be a great encouragement to the developers.
+        </p>
+        <p>
+          It works like this: you donate, then we are happy and motivated, then we improve OneSchedule, then you visualize your schedule better, then you are happy, then you approach your Meaning of Life.
+        </p>
+        <p>
+          Also, we need financial support to deliver more features: nicer domain, classroom map, credit planning, course review, and maybe an ecosystem of apps for Fulbright students. 
+          See our issues and upcoming features <a href="https://github.com/npnkhoi/oneschedule/issues">here</a>. We simply need extrinsic motivation to deliver them.
+        </p>
+        <p>Please consider donating to OneSchedule (e.g., 20K VND) via 2 following channels:</p>
+        <ol className="border border-primary rounded py-3">
+          <li>ACB: 7132717 (NGUYEN PHUNG NHAT KHOI, Branch: Nam Sai Gon)
+          </li>
 
-              <li>Momo: 0343059217 (NGUYEN PHUNG NHAT KHOI)
-              </li>
-            </ol>
-          </div>
-          <p>Don&apos;t forget to leave me a message when you donate. You support will be remembered forever.</p>
-        </div>
+          <li>Momo: 0343059217 (NGUYEN PHUNG NHAT KHOI, don&apos;t judge my avatar)
+          </li>
+        </ol>
+        <p>When you give, don&apos;t forget to leave us a message. You support will be remembered forever.</p>
+        {/* <div className="rounded border p-3">
+        </div> */}
 
-        <div className='border rounded p-3'>
-          <h5>2. Give feedback</h5>
-          <p>If you find a bug in the app, or simpy want to let us know your thoughts, we would love to here it. That is really the best and easiest way to support this app.</p>
-          <CTA
-            text='Give feedback'
-            link='https://docs.google.com/forms/d/e/1FAIpQLScIRIBsSk_ciXC_peeanPJXyeh7MB56eaGhOvNWWoznSaM_6g/viewform'
-          />
-        </div>
-        <span>Thanks a ton - Khoi</span>
+        <h5>2. Give feedback</h5>
+        <p>If you find a bug in the app, or simpy want to let us know your thoughts, we would love to here it. That is really the easiest way to support this app.</p>
+        <CTA
+          text='Give feedback'
+          link='https://docs.google.com/forms/d/e/1FAIpQLScIRIBsSk_ciXC_peeanPJXyeh7MB56eaGhOvNWWoznSaM_6g/viewform'
+        />
+        <p>
+          Cheers,
+          <br />
+          Khoi
+        </p>
       </div>
     </div>
   )
