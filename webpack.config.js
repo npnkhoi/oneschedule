f9267399c157df2c49608c0eec2bbf88090c6f1f
npnkhoi <npnkhoi@gmail.com>
2022-10-30 23:52:07 +0700

    config for TS

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+module.exports = {
+    // change to .tsx if necessary
+    entry: './src/index.js',
+    output: {
+      filename: './bundle.js'
+    },
+    resolve: {
+      // changed from extensions: [".js", ".jsx"]
+      extensions: [".ts", ".tsx", ".js", ".jsx", ".css", ".scss"],
+      fallback: {
+        fs: false
+      }
+    },
+    module: {
+      rules: [
+        // changed from { test: /\.jsx?$/, use: { loader: 'babel-loader' }, exclude: /node_modules/ },
+        { test: /\.(t|j)sx?$/, use: { loader: 'ts-loader' }, exclude: /node_modules/ },
+  
+        // addition - add source-map support
+        { enforce: "pre", test: /\.js$/, exclude: /node_modules/, loader: "source-map-loader" },
+
+        // { test: /\.js$/, exclude: /node_modules/, loaders: 'babel', query: { presets: ['react', 'es2015', 'stage-1'] } },
+        { test: /\.css$/, use: ["style-loader", "css-loader"] },
+        {
+            test: /\.scss$/,
+            use: [{
+                loader: "style-loader"
+            }, {
+                loader: "css-loader"
+            }, {
+                loader: "sass-loader",
+            }]
+        }
+      ]
+    },
+    externals: {
+      "react": "React",
+      "react-dom": "ReactDOM",
+    },
+    // addition - add source-map support
+    devtool: "source-map",
+  }
