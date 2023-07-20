aaa16f035d20b8e7f54483c4f67944962a3648a4
lanphgphm <phuong.pham.210120@student.fulbright.edu.vn>
2023-07-20 17:23:14 +0700

    updated fall23 schedule (still no room)

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,5 +1,4 @@
 import COLORS from './colors.json'
-import Spring2023 from './2022-2023 Spring 2023 Term.json'
 import Summer2023 from './2022-2023 Summer Term.json'
 import Fall2023 from './2023-2024 Fall 2023 Term.json'
 
