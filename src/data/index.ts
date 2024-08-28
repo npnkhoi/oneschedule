4d58e1836f52ac8947d5335d91c33edf2b52e952
lanphgphm <phuong.pham.210120@student.fulbright.edu.vn>
2024-08-28 18:22:59 +0700

    opening all term data

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,4 +1,10 @@
 import COLORS from './colors.json'
+import Fall2021 from './2021-2022 Fall 2021 Term.json'
+import Spring2022 from './2021-2022 Spring 2022 Term.json'
+import Summer2022 from './2021-2022 Summer 2022 Term.json'
+import Fall2022 from './2022-2023 Fall 2022 Term.json'
+import Spring2023 from './2022-2023 Spring 2023 Term.json'
+import Summer2023 from './2022-2023 Summer Term.json'
 import Fall2023 from './2023-2024 Fall 2023 Term.json'
 import Spring2024 from './2023-2024 Spring 2024 Term.json'
 import Fall2024 from './2024-2025 Fall 2024 Term.json'
@@ -19,7 +25,7 @@ export interface Course {
     location?: string;
 }
 
-const courseData: {[key: string]: Course[]} = {Fall2023, Spring2024, Fall2024}
-const availableTerms: string[] = ['Fall2023', 'Spring2024', 'Fall2024']
+const courseData: {[key: string]: Course[]} = {Fall2021, Spring2022, Summer2022, Fall2022, Spring2023, Summer2023, Fall2023, Spring2024, Fall2024}
+const availableTerms: string[] = ['Fall2021', 'Spring2022', 'Summer2022', 'Fall2022', 'Spring2023', 'Summer2023', 'Fall2023', 'Spring2024', 'Fall2024']
 
 export {COLORS, courseData, availableTerms}
